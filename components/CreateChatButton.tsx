06b0f3a302e5153240c960497cecc0af11f478c2
fernando <contato@frinfo.com.br>
2023-11-23 01:50:40 -0300

    feat: Include initial page and pricing page

--- components/CreateChatButton.tsx
+++ components/CreateChatButton.tsx
@@ -1,8 +1,20 @@
 'use client'
 
+import {Button} from "@/components/ui/button";
+import {MessageSquarePlusIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
+
 function CreateChatButton(){
+  const router = useRouter()
+  const createNewChat = async() => {
+    // .. Logic to create new chat
+
+    router.push(`/chat/abc}`)
+  }
   return (
-    <div></div>
+    <Button onClick={createNewChat} variant={'ghost'}>
+      <MessageSquarePlusIcon className="text-black dark:text-white" />
+    </Button>
   )
 }
 
---
+++ components/PricingCards.tsx
@@ -0,0 +1,7 @@
+function PricingCards({redirect} : {redirect: boolean}) {
+  return (
+    <div></div>
+  )
+}
+
+export default PricingCards
