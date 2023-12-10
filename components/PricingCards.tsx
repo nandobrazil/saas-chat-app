cdc7b1981813f2a1a37aca6838bc010ea5c0b2b5
Fernando Conde <contato@frinfo.com.br>
2023-12-09 21:40:46 -0300

    feat: Add pricing cards

--- components/PricingCards.tsx
+++ components/PricingCards.tsx
@@ -1,7 +1,86 @@
+import {CheckIcon} from "lucide-react";
+
+const tiers = [
+  {
+    id: null,
+    name: 'Starter',
+    href: '#',
+    priceMonthly: null,
+    description: 'Get chatting right way with anyone, anywhere!',
+    features: [
+      '20 Message Chat Limit in Chats',
+      '2 Participants limit in Chat',
+      '3 Chat Rooms limit',
+      'Supports 2 languages',
+      '48-hour support response time'
+    ],
+  },
+  {
+    id: 'ID_ABCABC',
+    name: 'Pro',
+    href: '#',
+    priceMonthly: 'R$ 9,90',
+    description: 'Unlock the Full Potential with Pro!',
+    features: [
+      'Unlimited Messages in Chats',
+      'Unlimited Participants in Chat',
+      'Unlimited Chat Rooms',
+      'Supports up to 10 languages',
+      'Multimedia support in chats (cooming soon)',
+      '1-hour, dedicated support response time',
+      'Early access to new features'
+    ],
+  }
+]
+
 function PricingCards({redirect} : {redirect: boolean}) {
-  return (
-    <div></div>
-  )
+  return <div>
+    <div className="mx-auto grid max-w-md grid-cols-1 gap-8 lg:max-w-4xl lg:grid-cols-2">
+      {tiers.map((tier) => (
+        <div key={tier.name}
+             className="flex flex-col justify-between rounded-3xl bg-white p-8 shadow-xl ring-1 ring-gray-900/10 sm:pm-10"
+        >
+          <div>
+            <div className="mt-4 flex items-baseline gap-x-2">
+              {tier.priceMonthly ? (
+                  <>
+                    <span className="text-5xl font-bold tracking-tight text-gray-900">
+                      {tier.priceMonthly}
+                    </span>
+                    <span className="text-base font-semibold leading-7 text-gray-600">/month</span>
+                  </>
+                ) :
+                (
+                  <>
+                    <span className="text-5xl font-bold tracking-tight text-gray-900">
+                      Free
+                    </span>
+                  </>
+                )
+              }
+            </div>
+            <p className="mt-6 text-base leading-7 text-gray-600">
+              {tier.description}
+            </p>
+            <ul
+              role="list"
+              className="mt-10 space-y-4 text-sm leading-6 text-gray-600"
+            >
+              {tier.features.map((feature) => (
+                <li key={feature} className="flex gap-x-3">
+                  <CheckIcon
+                    className="h-6 w-5 flex-none text-indigo-600"
+                    aria-hidden="true"
+                  />
+                  {feature}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      ))}
+    </div>
+  </div>
 }
 
 export default PricingCards
